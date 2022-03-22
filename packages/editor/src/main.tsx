46d601a31dad97fb7c0a739130b168678a7c88d4
LiYechao <liyechao@yechao.xyz>
2022-03-22 23:14:08 +0800

    feat: base editor support

--- packages/editor/src/main.tsx
+++ packages/editor/src/main.tsx
@@ -12,7 +12,89 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import styled from '@emotion/styled'
+import { useMemo } from 'react'
 import ReactDOM from 'react-dom'
-import Editor from './Editor'
+import Editor, {
+  baseKeymap,
+  Blockquote,
+  Bold,
+  BulletList,
+  Code,
+  Doc,
+  dropCursor,
+  gapCursor,
+  Heading,
+  Highlight,
+  history,
+  Italic,
+  keymap,
+  Link,
+  OrderedList,
+  Paragraph,
+  Plugins,
+  redo,
+  State,
+  Strikethrough,
+  Text,
+  Underline,
+  undo,
+  undoInputRule,
+  Value,
+} from './Editor'
 
-ReactDOM.render(<Editor />, document.getElementById('root'))
+const App = () => {
+  const state = useMemo(() => {
+    return new State({
+      nodes: [
+        new Doc(),
+        new Text(),
+        new Paragraph(),
+        new Heading(),
+        new Blockquote(),
+        new OrderedList(),
+        new BulletList(),
+      ],
+      marks: [
+        new Bold(),
+        new Code(),
+        new Highlight(),
+        new Italic(),
+        new Link(),
+        new Strikethrough(),
+        new Underline(),
+      ],
+      extensions: [
+        new Plugins([
+          keymap({
+            'Mod-z': undo,
+            'Shift-Mod-z': redo,
+            'Mod-y': redo,
+            Backspace: undoInputRule,
+          }),
+          keymap(baseKeymap),
+          history(),
+          gapCursor(),
+          dropCursor({ color: 'currentColor' }),
+        ]),
+        new Value({
+          defaultValue: {
+            type: 'doc',
+            content: [
+              { type: 'heading', attrs: { level: 1 }, content: [{ type: 'text', text: 'HELLO' }] },
+            ],
+          },
+        }),
+      ],
+    })
+  }, [])
+
+  return <_Editor state={state} />
+}
+
+const _Editor = styled(Editor)`
+  border: 1px solid #efefef;
+  min-height: 80vh;
+`
+
+ReactDOM.render(<App />, document.getElementById('root'))
