38b19906748e406d36c80c3f56555c95d97271c8
LiYechao <liyechao@yechao.xyz>
2022-03-25 21:00:50 +0800

    fix: add array polyfills

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      renderLegacyChunks: false,
+      modernPolyfills: ['es/array'],
+    }),
+  ],
 })
