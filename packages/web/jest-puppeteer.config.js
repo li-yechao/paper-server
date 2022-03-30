f375d4e4bbd36b0fe5f2c2f2530f920d4683c60d
LiYechao <liyechao@yechao.xyz>
2022-03-30 13:01:22 +0800

    chore: use vite dev mode to test

--- packages/web/jest-puppeteer.config.js
+++ packages/web/jest-puppeteer.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   server: {
-    command: 'yarn vite preview --port 4444',
+    command: 'yarn vite --port 4444 --strictPort',
+    port: 4444,
+    host: '127.0.0.1',
+    protocol: 'tcp',
+    usedPortAction: 'kill',
   },
   browserContext: 'incognito',
 }
