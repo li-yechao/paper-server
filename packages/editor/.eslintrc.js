5af823e28d7b1be63ff3e09af170e28151cae4d6
LiYechao <liyechao@yechao.xyz>
2022-10-07 14:14:24 +0800

    chore: remove web packages

--- packages/editor/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['../../.eslintrc'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
